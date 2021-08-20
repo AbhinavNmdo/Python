c6a62271a53e279538154abaca430d63672cdd22
Abhinav Namdeo <abhaynam22@gmail.com>
2021-08-20 10:36:00 +0530

    Initial Commit

--- Ex09_AkhbaarPadhkeSunao.py
+++ Ex09_AkhbaarPadhkeSunao.py
@@ -1,6 +1,21 @@
+import json
+
+import requests
+
+
 def speak(str):
     from win32com.client import Dispatch
     speak = Dispatch("SAPI.SpVoice")
+    print("Speaking.....")
     speak.Speak(str)
 
-speak("How are you brother")
+if __name__ == '__main__':
+    url = "https://newsapi.org/v2/everything?q=Apple&from=2021-08-20&sortBy=popularity&apiKey=33e7205e2fe547089f0fe1cb9e41f4bc"
+    news = requests.get(url).text
+    news_json = json.loads(news)
+    arts = news_json["articles"]
+    speak("News for today")
+    for article in arts:
+        speak(article["title"])
+        speak("Next news, listen carefully")
+
