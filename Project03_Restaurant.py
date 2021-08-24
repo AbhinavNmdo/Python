afe9a29bdfa7a0cfcb8fc232cdb3486f6649835f
Abhinav Namdeo <abhaynam22@gmail.com>
2021-08-24 17:22:39 +0530

    Initial Commit

--- Project03_Restaurant.py
+++ Project03_Restaurant.py
@@ -1,8 +1,19 @@
 food = []
-for i in range(3) :
+for i in range(5):
     inp = input("Enter the Food Name\n")
-    if inp == "exit":
-        exit()
-    else:
-        food.append(inp)
-print(food)
+    food.append(inp)
+print("Reverse by inbuilt python method")
+foodwithreverse = food
+foodwithreverse.reverse()
+print(foodwithreverse)
+print("----------------------------------------------------")
+print("Reverse by Slicing")
+foodwithslicing = food
+foodwithslicing[::-1]
+print(foodwithslicing)
+print("----------------------------------------------------")
+print("Reverse by using Swapping method")
+foodwithswapping = food
+for i in range(len(foodwithswapping)//2):
+    foodwithswapping[i], foodwithswapping[len(foodwithswapping) -i -1] = foodwithswapping[len(foodwithswapping) -i -1], foodwithswapping[i]
+print(foodwithswapping)
